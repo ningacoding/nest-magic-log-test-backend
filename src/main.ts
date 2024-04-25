939f35b8c07faea865762d7aa8b045aa115c38d8
Kuriel <ikuriel@gmail.com>
2024-04-24 22:09:01 -0600

    Access-Control-Allow-Origin *

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,24 @@ async function bootstrap() {
   });
   app.enableCors();
 
+  app.use(
+    (
+      req: any,
+      res: { header: (arg0: string, arg1: string) => void },
+      next: () => void,
+    ) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    },
+  );
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('MagicLog Test')
     .setDescription('The MagicLog Test API')
